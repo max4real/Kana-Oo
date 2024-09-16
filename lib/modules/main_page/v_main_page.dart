a64e7a0628cb3458ec055692f3bcca5b5f8d7468
Myint Myat Soe <max@max-mac.local>
2024-09-16 10:19:02 +0630

    bottom navi bar done

--- lib/modules/main_page/v_main_page.dart
+++ lib/modules/main_page/v_main_page.dart
@@ -15,132 +15,102 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: background,
-      bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            color: onBackground,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          margin: const EdgeInsets.only(
-            left: 15,
-            right: 15,
-            bottom: 15,
-          ),
-          height: 55,
-          child: ValueListenableBuilder(
-            valueListenable: controller.index_,
-            builder: (context, value, child) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    splashRadius: 5,
-                    isSelected: value == 0 ? true : false,
-                    selectedIcon: Icon(
-                      Iconsax.home_1,
-                      color: secondary,
-                    ),
-                    onPressed: () {
-                      controller.index_.value = 0;
-                    },
-                    icon: const Icon(
-                      Iconsax.home,
-                      color: Colors.grey,
-                    ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ValueListenableBuilder(
+              valueListenable: controller.index_,
+              builder: (context, value, child) {
+                return shownePage(value);
+              },
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: onBackground,
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  IconButton(
-                    isSelected: value == 1 ? true : false,
-                    selectedIcon: Icon(
-                      Iconsax.shopping_bag,
-                      color: secondary,
-                    ),
-                    onPressed: () {
-                      controller.index_.value = 1;
-                    },
-                    icon: const Icon(
-                      Iconsax.shopping_bag,
-                      color: Colors.grey,
-                    ),
+                  margin: const EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                    bottom: 15,
                   ),
-                  IconButton(
-                    isSelected: value == 2 ? true : false,
-                    selectedIcon: Icon(
-                      Iconsax.heart,
-                      color: secondary,
-                    ),
-                    onPressed: () {
-                      controller.index_.value = 2;
+                  height: 60,
+                  child: ValueListenableBuilder(
+                    valueListenable: controller.index_,
+                    builder: (context, value, child) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            splashRadius: 5,
+                            isSelected: value == 0 ? true : false,
+                            selectedIcon: Icon(
+                              Iconsax.home_1,
+                              color: secondary,
+                            ),
+                            onPressed: () {
+                              controller.index_.value = 0;
+                            },
+                            icon: const Icon(
+                              Iconsax.home,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          IconButton(
+                            isSelected: value == 1 ? true : false,
+                            selectedIcon: Icon(
+                              Iconsax.shopping_bag,
+                              color: secondary,
+                            ),
+                            onPressed: () {
+                              controller.index_.value = 1;
+                            },
+                            icon: const Icon(
+                              Iconsax.shopping_bag,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          IconButton(
+                            isSelected: value == 2 ? true : false,
+                            selectedIcon: Icon(
+                              Iconsax.heart,
+                              color: secondary,
+                            ),
+                            onPressed: () {
+                              controller.index_.value = 2;
+                            },
+                            icon: const Icon(
+                              Iconsax.heart,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          IconButton(
+                            isSelected: value == 3 ? true : false,
+                            selectedIcon: Icon(
+                              Iconsax.setting,
+                              color: secondary,
+                            ),
+                            onPressed: () {
+                              controller.index_.value = 3;
+                            },
+                            icon: const Icon(
+                              Iconsax.setting,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      );
                     },
-                    icon: const Icon(
-                      Iconsax.heart,
-                      color: Colors.grey,
-                    ),
                   ),
-                  IconButton(
-                    isSelected: value == 3 ? true : false,
-                    selectedIcon: Icon(
-                      Iconsax.setting,
-                      color: secondary,
-                    ),
-                    onPressed: () {
-                      controller.index_.value = 3;
-                    },
-                    icon: const Icon(
-                      Iconsax.setting,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              );
-            },
-          )),
-      body: ValueListenableBuilder(
-        valueListenable: controller.index_,
-        builder: (context, value, child) {
-          return shownePage(value);
-        },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-
-      // bottomNavigationBar: Obx(() {
-      //   return BottomNavigationBar(
-      //     backgroundColor: onBackground,
-      //     elevation: 0,
-      //     currentIndex: controller.selectedIndex.value,
-      //     onTap: (value) {
-      //       controller.selectedIndex.value = value;
-      //     },
-      //     items: const [
-      //       BottomNavigationBarItem(
-      //         icon: Icon(
-      //           Iconsax.home,
-      //           color: Colors.white,
-      //         ),
-      //         // label: "HOME",
-      //         // selectedIcon: Icon(Iconsax.home),
-      //       ),
-      //       BottomNavigationBarItem(
-      //         icon: Icon(
-      //           Iconsax.shopping_bag,
-      //           color: Colors.white,
-      //         ),
-      //         // label: "CART",
-      //         // selectedIcon: Icon(Iconsax.shopping_bag),
-      //       ),
-      //       BottomNavigationBarItem(
-      //         icon: Icon(
-      //           Iconsax.heart,
-      //           color: Colors.white,
-      //         ),
-      //         // label: "WISH LIST",
-      //         // selectedIcon: Icon(Iconsax.heart),
-      //       ),
-      //     ],
-      //   );
-      // }),
-      // body: Obx(
-      //   () {
-      //     return shownePage();
-      //   },
-      // ),
     );
   }
 
